9b3871b988cc36dee42d0505ab4b1f1be18fe09c
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-10-11 20:08:36 +0000

    ran migration for users

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
   def new
     @movie = Movie.new
   end
@@ -14,12 +16,9 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params.fetch(:id))
   end
 
-  def create
-    movie_params = params.require(:movie).permit(:title, :description)
-    
+  def create    
     @movie = Movie.new(movie_params)
 
     if @movie.valid?
@@ -32,13 +31,9 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def update
-    @movie = Movie.find(params.fetch(:id))
-
-    movie_params = params.require(:movie).permit(:title, :description)
     
     if @movie.update(movie_params)
       redirect_to @movie, notice: "Movie updated successfully."
@@ -48,10 +43,18 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params.fetch(:id))
-
     @movie.destroy
 
     redirect_to movies_url, notice: "Movie deleted successfully."
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :image_url, :released_on)
+  end
+
+  def set_movie
+    @movie = Movie.find(params.fetch(:id))
   end
 end
